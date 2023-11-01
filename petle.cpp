af813680565d765e8febb278bbebae6f91d7c44c
Blazej Leman <blazejleman789@gmail.com>
2023-11-01 23:03:43 +0100

    add printString() v0.1

--- petle.cpp
+++ petle.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 
 // Write your function here
+void printString(std::string txt, int num) {
+    for(int i = 0; i < num; i++) {
+        std::cout << txt << std::endl;
+    }
+    std::cout << "\n";
+}
 
 int main() {
     printString("Hello", 5);
-    std::cout << "\n";
-
     printString("AbC", 2);
-    std::cout << "\n";
-
     printString("HiHi ", 6);
-    std::cout << "\n";
-
     return 0;
 }
